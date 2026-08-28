c4e06540d12e3ec673ebc9dcdcdb19900610ae7d
iSlavik <williamssmith251@outlook.com>
2026-08-28 05:39:57 +0300

    Optimized auto run script

--- run.sh
+++ run.sh
@@ -1,51 +1,45 @@
 #!/bin/bash
-# github.com/islavikfx
-
-cd ~
-
+#!github.com/islavikfx
 
 if [ "$(whoami)" != "root" ]; then
     echo "> Abort. Run script with sudo."
     exit 1
 fi
 
-
 if ! pgrep -f cs2 > /dev/null; then
     echo "> Abort. Run CS2 before script."
     exit 1
 fi
 
-
 if [ ! -f /tmp/m.tmpf ]; then
     echo "> [+] Installing libs.."
     apt update && apt install -y git cmake build-essential
+    touch /tmp/m.tmpf
 fi
-
-touch /tmp/m.tmpf
 
 ver=$(find /home -name "steam.inf" 2>/dev/null | grep "csgo" | head -1 | xargs -d '\n' grep "PatchVersion" 2>/dev/null | cut -d'=' -f2)
-echo "CS2 version: $ver."
+ver=$(echo "$ver" | tr -d '\r')
+echo "CS2 version: $ver"
 
+who=$(getent passwd 1000 | cut -d: -f1)
+dirc="/home/$who"
 
-if [ ! -d "Linux-CS2-Dumper" ]; then
-    git clone https://github.com/islavikfx/Linux-CS2-Dumper.git
-    cd Linux-CS2-Dumper/
-    mkdir -p build
-    cd build/
-    cmake ..\\
-    make -j$(nproc)
-    sudo ./CS2DumperBeta
-else
-    cd Linux-CS2-Dumper/
-    cd build/
-    rm -rf output/*
-    sudo ./CS2DumperBeta
-fi
+rm -rf /root/Linux-CS2-Dumper
+rm -rf "$dirc/Linux-CS2-Dumper"
 
+git clone https://github.com/islavikfx/Linux-CS2-Dumper.git "$dirc/Linux-CS2-Dumper"
 
-cd ~/Linux-CS2-Dumper/build/output/
-chmod 666 libclient_dump.cs
+cd "$dirc/Linux-CS2-Dumper"
+mkdir -p build
+cd build
+cmake ..
+make -j$(nproc)
+./CS2DumperBeta
+
+cd "$dirc/Linux-CS2-Dumper/build/output/"
 mv "libclient_dump.cs" "libclient_dump(${ver}).cs"
-sed -i "s/Input/$ver/g" "libclient_dump(${ver}).cs"
+sed -i "s/\[Input\]/$ver/g" "libclient_dump(${ver}).cs"
 
-echo "Done!"
+chmod 666 "$dirc/Linux-CS2-Dumper"
+chown -R "$who:$who" "$dirc/Linux-CS2-Dumper"
+echo "[+] File: $dirc/Linux-CS2-Dumper/build/output/libclient_dump(${ver}).cs"
